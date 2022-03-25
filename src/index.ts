8e3c90c6fd8a549fa51513b7641b11244042b77d
poshang <2890122341@qq.com>
2022-03-25 17:17:29 +0800

    2

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as http from 'http';
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}).listen(8888);
