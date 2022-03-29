761d10785b1e1488b7e50f9513921a5eda1dddf5
poshang <2890122341@qq.com>
2022-03-29 09:25:50 +0800

    3

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
+    console.log("hello");
+    
 }).listen(8888);
